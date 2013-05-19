3ac8ff1c4315b3c59685d8ee03369509df9453c3
James Pirruccello <james@carbocation.com>
2013-05-18 18:21:38 -0700

    First commit, must not upload to github due to auth info.

---
+++ continuous-compile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script keeps watch on the current project and compiles it continuously as you change files.
+# If there are multiple projects with the same final directory name (e.g., /proj/rad and /lib/monster/rad), 
+# this will kill any other similarly-named running projects' binaries, potentially leading to havoc.
+ 
+# To run, execute the following:
+# /usr/local/bin/fswatch ./ ./continuous-compile.sh
+
+echo 'Re-compiling with these options: '"$@"
+
+#Count the number of times the compiled app is running
+numproc=`ps aux | grep ${PWD##*/}-main.osx | grep -v grep | wc -l`
+
+if [[ numproc > 0 ]]
+	then
+	killall ${PWD##*/}-main.osx 2> /dev/null
+fi
+
+# Build the binary, then
+# add in the static components with NRSC, then
+# launch the binary in the background
+GOMAXPROCS=4 go build "$@" -o /tmp/${PWD##*/}-main.osx *.go && ./nrsc-script /tmp/${PWD##*/}-main.osx "static templates" -q && /tmp/${PWD##*/}-main.osx &
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/carbocation/go-instagram"
+	"github.com/gorilla/mux"
+)
+
+func init() {
+	instagram.Initialize(&instagram.Cfg{
+		ClientID:     "dfd9ae5cf32b4cdd9afe0fd24500a86b",
+		ClientSecret: "508160f256fb411290e770acaae31856",
+		RedirectURL:  "http://combigram.carbocation.com/ig-redirect",
+	})
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	ig, err := instagram.NewInstagram()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	url := ig.Authenticate("basic")
+	log.Println(url)
+	http.Redirect(w, r, url, http.StatusFound)
+
+	return
+}
+
+func RedirectHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("inside redirect")
+	rbody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(string(rbody))
+	fmt.Fprintf(w, "%s", string(rbody))
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", hello)
+	r.HandleFunc("/redirect/instagram", RedirectHandler)
+	http.Handle("/", r)
+	http.ListenAndServe(":9999", nil)
+}
